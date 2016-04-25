06c5ec213f1aa792b79fc0732cfea0407928d6af
modium <rtql8d@gmail.com>
2016-04-25 17:59:24 -0600

    added Star Wars API test

---
+++ index.js
@@ -0,0 +1,36 @@
+exports.sanitize = function(word) {
+	// console.log('running');
+	return word.toLowerCase().replace(/-/g,' '); //replace hyphens globally
+}
+
+exports.tokenize = function(sentence) {
+	return sentence.split(' '); //this function returns an array
+}
+
+exports.info = function(callback) {
+	var https = require('https');
+
+	var options = {
+		host: 'swapi.co',
+		path: '/api/people/1/?format=json',
+		method: 'GET'
+	};
+
+	var str = ''; //initialize empty string to put data into later
+	
+	https.request(options, function(response) {
+		response.on('data', function(data) {
+			str += data;
+		})
+		
+		response.on('end', function() {
+			callback(JSON.parse(str));
+		})
+		
+		response.on('error', function(error) {
+			console.log(error);
+			callback(error);
+		})
+	})
+	.end();
+}
---
+++ test/indexSpec.js
@@ -0,0 +1,48 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+
+var functions = require('../index');
+
+describe('Sanitize', function() {
+	// beforeEach(function() {
+	// });
+		// console.log('Starting test');
+	
+	it('returns lowercase of a string', function() {
+		var inputfunctions = 'HELLO WORLD';
+		var outputfunctions = functions.sanitize(inputfunctions);
+		
+		expect(outputfunctions).to.equal('hello world');
+		expect(outputfunctions).to.not.equal('HELLO WORLD');
+		expect(outputfunctions).to.be.a('string');
+		expect(outputfunctions).to.not.be.a('number');
+		expect(outputfunctions).to.contain('hello');
+	});
+	
+	it('removes any hyphens', function() {
+		var inputfunctions = 'HELLO-WORLD';
+		var outputfunctions = functions.sanitize(inputfunctions);
+		
+		expect(outputfunctions).to.equal('hello world');		
+	});
+})
+
+describe('Tokenize', function() {
+	it('returns an array of functions', function() {
+		var sentence = 'hello world';
+		var tokenizedSentence = functions.tokenize(sentence);
+		
+		expect(tokenizedSentence).to.include.members( [ 'hello', 'world' ] );
+	})
+})
+
+describe('Star Wars info', function() {
+	it.only('returns info from the Star Wars API', function(done) {
+		functions.info(function(reply){
+			console.log(reply);
+			expect(reply.name).to.equal('Luke Skywalker');
+			expect(reply.height).to.equal('172');
+			done();
+		})
+	})
+})
