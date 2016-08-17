24d8c6ae6556d9385bcdd159199e90fb0ed6e80d
modium <rtql8d@gmail.com>
2016-08-17 11:46:45 -0600

    added mocha documentation

--- webdriver/server.js
+++ webdriver/server.js
@@ -5,6 +5,7 @@ var http = require('http'),
 fs.readFile('./index.html', function(err, html) {
 	if(err) {
 		throw err;
+		//return console.log('An error occurred', err)
 	}
 
 	http.createServer(function(request, response) {
@@ -18,3 +19,21 @@ fs.readFile('./index.html', function(err, html) {
 });
 
 console.log('Server running at localhost:8000');
+
+// const http = require('http')
+// const port = 3000
+
+// const requestHandler = (request, response) => {
+	// console.log(request.url)
+	// response.end('Hello Node.js server')
+// }
+
+// const server = http.createServer(requestHandler)
+
+// server.listen(port, (err) -> {
+	// if (err) {
+		// return console.log('An error occurred', err)
+	// }
+	
+	// console.log('Server is listening on port ${3000}')
+// })
